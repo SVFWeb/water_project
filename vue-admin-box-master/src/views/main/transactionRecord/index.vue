df8e2b4b63785bf544ba968e99881b37cf04e40f
No_Star <105438922+SVFWeb@users.noreply.github.com>
2025-11-21 14:30:28 +0800

    更新 index.vue

--- vue-admin-box-master/src/views/main/transactionRecord/index.vue
+++ vue-admin-box-master/src/views/main/transactionRecord/index.vue
@@ -1,29 +1,17 @@
 <template>
   <div class="layout-container">
     <div class="layout-container-form flex space-between">
-
-      <div class="layout-container-form-handle">
-
-        <!-- 批量删除 -->
-        <el-popconfirm title="确定删除选中的数据吗？" @confirm="handleDel(chooseData)">
-          <template #reference>
-            <el-button type="danger" :icon="Delete" :disabled="chooseData.length === 0">批量删除</el-button>
-          </template>
-        </el-popconfirm>
-      </div>
-
       <!-- 搜索 -->
       <div class="layout-container-form-search">
         <el-input v-model="query.input" placeholder="请输入搜索的订单编号" @change="getTableData(true)"></el-input>
         <el-button type="primary" :icon="Search" class="search-btn" @click="getTableData(true)">搜索</el-button>
       </div>
-
     </div>
 
     <!-- 数据表格 -->
     <div class="layout-container-table">
-      <Table ref="table" v-model:page="page" v-loading="loading" :showIndex="true" :showSelection="true"
-        :data="tableData" @getTableData="getTableData" @selection-change="handleSelectionChange">
+      <Table ref="table" v-model:page="page" v-loading="loading" :showIndex="true"
+        :data="tableData" @getTableData="getTableData">
         <el-table-column prop="transactionId" label="订单编号" align="center" />
         <el-table-column prop="userName" label="用户" align="center" />
         <el-table-column prop="machineId" label="设备" align="center" />
@@ -59,11 +47,7 @@ import { ref, reactive, onMounted } from 'vue'
 import dayjs from 'dayjs'
 import Table from '@/components/table/index.vue'
 import { apiGetTransactionRecord, apiDeleteTransactionRecordId } from '@/api/transaction/record'
-//import { Page } from '@/components/table/type'
-// import { ElMessage } from 'element-plus'
-import { Search, Delete } from '@element-plus/icons'
-// import { selectData, radioData } from './enum'
-
+import { Search } from '@element-plus/icons'
 // 表格数据
 const tableData = ref([])
 // 存储搜索用的数据
@@ -78,8 +62,6 @@ const page = reactive({
 })
 // 表格加载状态
 const loading = ref(true)
-// 选择删除的数据
-const chooseData = ref([])
 
 // 获取列表数据
 async function getTransactionRecord() {
@@ -88,7 +70,6 @@ async function getTransactionRecord() {
     pageSize: page.size,
   })
   tableData.value = res.data.list
-
 }
 
 // 获取表格数据
@@ -96,9 +77,6 @@ const getTableData = () => {
   loading.value = false
 }
 
-function handleSelectionChange(value){
-  chooseData.value=value
-}
 
 // 删除功能
 const handleDel = async (data) => {
